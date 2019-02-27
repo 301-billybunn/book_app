d3a13ab3dbcd703d192d4ed7ad012a6d07cd93d1
Billy Bunn <williammbunn@gmail.com>
2019-02-27 10:00:57 -0800

    Creates database and schema

---
+++ data/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(255),
+  title VARCHAR(255),
+  isbn NUMERIC(13, 0),
+  image_url VARCHAR(255),
+  descript VARCHAR(65535),
+  bookshelf VARCHAR(255)
+);
